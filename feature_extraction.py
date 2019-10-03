0c5a2e4555bfc65b78565516460c2dd0b6ad8114
Dylan Shen <40523285+Dylansppy@users.noreply.github.com>
2019-10-04 02:26:15 +1300

    Add files via upload

---
+++ feature_extraction.py
@@ -0,0 +1,101 @@
+import os
+import time
+import numpy as np
+from multiprocessing import Pool
+from keras.preprocessing import image
+import cv2
+from keras.models import Model
+from keras.applications.xception import Xception
+from keras.applications.xception import preprocess_input as xception_preprocess_input
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import preprocess_input as vgg16_preprocess_input
+from keras.applications.vgg19 import VGG19
+from keras.applications.vgg19 import preprocess_input as vgg19_preprocess_input
+from preprocessing import list_pictures
+
+
+#Organizing the dataset
+image_dir = os.path.join('..', 'data', 'image_processed')
+magnification_factors = ['40', '100', '200', '400']
+
+model1 = Xception(weights='imagenet', include_top=False) #imports the Xception model and discards the last classification layer.
+model2 = VGG16(weights='imagenet', include_top=False) #imports the VGG16 model and discards the last classification layer.
+base_model = VGG19(weights='imagenet')
+model3 = Model(inputs=base_model.input, outputs=base_model.get_layer('block4_pool').output) #imports the VGG19 model and discards the last classification layer.
+models = {'xception': (model1, xception_preprocess_input),
+          'vgg16': (model2, vgg16_preprocess_input),
+          'vgg19': (model3, vgg19_preprocess_input)
+          }
+
+
+# Define a function to extract features using pre-trained network
+def feature_extraction(model_name, img_path, magnification_factor, input_shape, out_path):
+    features = []
+    labels = []
+    patients = []
+    for img_dir in list_pictures(img_path, magnification_factor, ext='.npy'):
+        x = np.load(img_dir) # 460*700*7
+        x = cv2.resize(x, dsize=input_shape, interpolation=cv2.INTER_CUBIC) #(input_size, 7)
+        # The network expects one or more images as input; that means the input array will need to be 4-dimensional:
+        # samples, rows, columns, and channels.  channel number of 3 is required for pre-trained model
+        x = np.expand_dims(x, axis=0)
+        x = models[model_name][1](x)
+        feature = models[model_name][0].predict(x)
+        feature_array = np.array(feature, dtype=float)
+        features.append(feature_array)
+        directory, file_name = os.path.split(img_dir)
+        if file_name.split('_')[1] == 'B':
+            label = 0
+        elif file_name.split('_')[1] == 'M':
+            label = 1
+        labels.append(label)
+        patients.append(file_name.split('-')[2])
+    X = np.asarray(features)
+    y = np.asarray(labels)
+    p = np.asarray(patients)
+    feature_path = os.path.join(out_path, magnification_factor)
+    if not os.path.exists(feature_path):
+        os.makedirs(feature_path)
+    np.save(os.path.join(feature_path, 'X.npy'), X)
+    np.save(os.path.join(feature_path, 'y.npy'), y)
+    np.save(os.path.join(feature_path, 'p.npy'), p)
+
+
+# Deploy feature extraction
+def feature_extraction_deploy(model_name, input_shape, output_dir, input_dir=image_dir):
+    multi1 = time.time()
+    print('Parent process {0}'.format(os.getpid()))
+    print('Waiting for all processes done...')
+    p = Pool(4)
+    for i in range(4):
+        p.apply_async(feature_extraction,
+                      args=(model_name,
+                            input_dir,
+                            magnification_factors[i],
+                            input_shape,
+                            output_dir
+                ))
+    p.close()
+    p.join()
+    print('All processes done.')
+    multi2 = time.time()
+    print('Time comsumed:', multi2 - multi1)
+
+if __name__ == "__main__":
+    # Extract features with Xception, the default input size for this model is 299x299.
+    input_shape = (299, 299)
+    out_path_xception = os.path.join('..', 'data', 'features_xception')
+    feature_extraction_deploy('xception', input_shape, out_path_xception)
+
+    # Extract features with VGG16, the default input size for this model is 224x224.
+    input_shape = (224, 224)
+    out_path_vgg16 = os.path.join('..', 'data', 'features_vgg16')
+    feature_extraction_deploy('vgg16', input_shape, out_path_vgg16)
+
+    # Extract features from an arbitrary intermediate layer with VGG19, the default input size for this model is 224x224
+    input_shape = (224, 224)
+    out_path_vgg19 = os.path.join('..', 'data', 'features_vgg19')
+    feature_extraction_deploy('vgg19', input_shape, out_path_vgg19)
+
+
+
--- preprocessing.py
+++ preprocessing.py
@@ -3,25 +3,36 @@ import cv2
 import numpy as np
 from multiprocessing import Pool
 from tqdm import tqdm
-from typing import List, Text
+from typing import List, Text, Dict
 
 # Setting the directory of train and test datasets
-TRAIN_IMAGE_DIR = os.path.join('..', 'data', 'breast', 'Train')
-TRAIN_IMAGE_PROCESSED = os.path.join('..', 'data', 'train_processed')
+IMAGE_DIR = os.path.join('..', 'data', 'BreaKHis_v1')
+IMAGE_PROCESSED = os.path.join('..', 'data', 'image_processed')
 
-TEST_IMAGE_DIR = os.path.join('..', 'data', 'breast', 'Test')
-TEST_IMAGE_PROCESSED = os.path.join('..', 'data', 'test_processed')
+def dic_patient_directory(directory: Text, ext) -> Dict:
+    result = {}
+    for root, _, files in os.walk(directory):
+        for f in files:
+            if f.endswith(ext):
+                patient = f.split('-')[2]
+                if patient in result:
+                    result[patient].append(os.path.join(root, f))
+                else:
+                    result[patient] = [os.path.join(root, f)]
+    return result
 
 
-def list_pictures(directory: Text, mag, ext='.png') -> List:
+def list_pictures(directory: Text, magnification_factor, ext) -> List:
     # return a list contains paths of all 400*400 histopathologic image 
     return [os.path.join(root, f)
             for root, _, files in os.walk(directory) for f in files
             if f.endswith(ext)
-            if f.split('-')[3] == mag]
+            if f.split('-')[3] == magnification_factor]
+
 
 def gaussian_blur(image):
     return cv2.GaussianBlur(image, (3, 3), cv2.BORDER_DEFAULT)
+
 
 def rgb_to_hsv(image):
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -35,41 +46,45 @@ def rgb_to_hsv(image):
     return hsv
 
 
-def preprocessing(image_dirs, target_dir, zoom):
-    for image_dir in tqdm(list_pictures(image_dirs, zoom)):
+def preprocessing(image_dirs, target_dir, magnification_factor, extension):
+    for image_dir in tqdm(list_pictures(image_dirs, magnification_factor, extension)):
         image = cv2.imread(image_dir)
-        image = gaussian_blur(image)
+        image = gaussian_blur(image)  #460*700*3
         # transfer the image from rgb to gray for histogram equlization
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        equ = cv2.equalizeHist(gray)
+        equ = cv2.equalizeHist(gray)  #460*700
+        equ = np.expand_dims(equ, axis=2)  #460*700*1
         # transfer the image from rgb to hsv
-        # hsv = rgb_to_hsv(image)
+        hsv = rgb_to_hsv(image)   #460*700*3
         # save the pre-processed image 
         directory, file_name = os.path.split(image_dir)
         #sub_directory = directory.split(os.sep)[4]
+        if not os.path.exists(target_dir):
+            os.makedirs(target_dir)
         #cv2.imwrite(os.path.join(target_dir, file_name), image)
-        #cv2.imwrite(os.path.join(target_dir, 'hsv_' + file_name), hsv)
-        cv2.imwrite(os.path.join(target_dir, file_name), equ)
+        #cv2.imwrite(os.path.join(target_dir, file_name), hsv)
+        #cv2.imwrite(os.path.join(target_dir, file_name), equ)
+        img_rgb_hsv_equ = np.concatenate((image, hsv, equ), axis=2) # 460*700*7
+        np.save(os.path.join(target_dir, file_name + '.npy'), img_rgb_hsv_equ)
         #k = cv2.waitKey(1005) & 0xFF
         #if k == 27:
         #    break
     cv2.destroyAllWindows()
 
+
 if __name__ == '__main__':
     magnification_factors = ['40', '100', '200', '400']
+    extension = '.png'
     multi1 = time.time()
     print('Parent process {0}'.format(os.getpid()))
     p = Pool(4)   # create four sub processes
     for i in range(4):
         p.apply_async(preprocessing,
-                      args=(os.path.join(TRAIN_IMAGE_DIR),
-                            os.path.join(TRAIN_IMAGE_PROCESSED, magnification_factors[i]),
-                            magnification_factors[i])
-                      )
-        p.apply_async(preprocessing,
-                      args=(os.path.join(TEST_IMAGE_DIR),
-                            os.path.join(TEST_IMAGE_PROCESSED, magnification_factors[i]),
-                            magnification_factors[i])
+                      args=(os.path.join(IMAGE_DIR),
+                            os.path.join(IMAGE_PROCESSED, magnification_factors[i]),
+                            magnification_factors[i],
+                            extension
+                            )
                       )
     print('Waiting for all subprocesses done...')
     p.close()
@@ -77,5 +92,3 @@ if __name__ == '__main__':
     print('All subprocesses done.')
     multi2 = time.time()
     print('Time comsumed:', multi2 - multi1)
-
-
